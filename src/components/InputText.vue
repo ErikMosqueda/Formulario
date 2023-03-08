91b93ff3fe0db0edb4132cdd5274f8bd46f0104c
Erik Mosqueda <erik.19070047@itsmotul.edu.mx>
2023-03-08 14:55:26 -0600

    Input de tipo texto como componente

---
+++ src/components/InputText.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    
+    data() {
+        return {
+            dato: 'PRUEBA DE ENVIO dato',
+        }
+    },
+    methods: {
+        enviar() {
+            dato: this.info,
+            this.$emit('informacion',this.dato)
+        }
+    }
+}
+</script>
+
+<template>
+    <div>
+        <slot></slot> ({{ dato}})
+        <br>
+        <input type="text" v-model="dato" name="info" id="info" class="px-2 py-1 rounded-lg mb-3 ml-2 mr-2" @input="enviar">
+
+    </div>
+</template>
